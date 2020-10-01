02ffd403da25187bf2c89285ed55da6ceb6291a7
Royce Dy <rad182@users.noreply.github.com>
2020-10-01 09:05:31 +0800

    Update RxApollo.swift

--- RxApollo/RxApollo.swift
+++ RxApollo/RxApollo.swift
@@ -9,20 +9,17 @@
 import Foundation
 import RxSwift
 import Apollo
+import AlamofireNetworkActivityIndicator
 
 /// An `Error` emitted by `ApolloReactiveExtensions`.
 public enum RxApolloError: Error {
     /// One or more `GraphQLError`s were encountered.
     case graphQLErrors([GraphQLError])
+    case unknown
 }
 
 /// Reactive extensions for `ApolloClient`.
-public final class ApolloReactiveExtensions {
-    private let client: ApolloClient
-
-    fileprivate init(_ client: ApolloClient) {
-        self.client = client
-    }
+extension Reactive where Base: ApolloClient {
 
     /// Fetches a query from the server or from the local cache, depending on the current contents of the cache and the specified cache policy.
     ///
@@ -30,56 +27,41 @@ public final class ApolloReactiveExtensions {
     ///   - query: The query to fetch.
     ///   - cachePolicy: A cache policy that specifies when results should be fetched from the server and when data should be loaded from the local cache.
     ///   - queue: A dispatch queue on which the result handler will be called. Defaults to the main queue.
-    /// - Returns: A `Maybe` that emits the results of the query.
-    public func fetch<Query: GraphQLQuery>(
-        query: Query,
-        cachePolicy: CachePolicy = .returnCacheDataElseFetch,
-        queue: DispatchQueue = DispatchQueue.main) -> Maybe<Query.Data> {
-        return Maybe.create { maybe in
-            let cancellable = self.client.fetch(query: query, cachePolicy: cachePolicy, queue: queue) { result, error in
-                if let error = error {
-                    maybe(.error(error))
-                } else if let errors = result?.errors {
-                    maybe(.error(RxApolloError.graphQLErrors(errors)))
-                } else if let data = result?.data {
-                    maybe(.success(data))
-                } else {
-                    maybe(.completed)
-                }
-            }
-
-            return Disposables.create {
-                cancellable.cancel()
-            }
-        }
-    }
-
-    /// Watches a query by first fetching an initial result from the server or from the local cache, depending on the current contents of the cache and the specified cache policy. After the initial fetch, the returned `Observable` will emit events whenever any of the data the query result depends on changes in the local cache.
-    ///
-    /// - Parameters:
-    ///   - query: The query to watch.
-    ///   - cachePolicy: A cache policy that specifies when results should be fetched from the server or from the local cache.
-    ///   - queue: A dispatch queue on which the result handler will be called. Defaults to the main queue.
-    /// - Returns: An `Observable` that emits the results of watching the `query`.
-    public func watch<Query: GraphQLQuery>(
+    /// - Returns: A `Observable` that emits the results of the query.
+    @discardableResult public func fetch<Query: GraphQLQuery>(
         query: Query,
         cachePolicy: CachePolicy = .returnCacheDataElseFetch,
         queue: DispatchQueue = DispatchQueue.main) -> Observable<Query.Data> {
-        return Observable.create { observer in
-            let watcher = self.client.watch(query: query, cachePolicy: cachePolicy, queue: queue) { result, error in
-                if let error = error {
-                    observer.onError(error)
-                } else if let errors = result?.errors {
-                    observer.onError(RxApolloError.graphQLErrors(errors))
-                } else if let data = result?.data {
-                    observer.onNext(data)
-                }
+        NetworkActivityIndicatorManager.shared.incrementActivityCount()
 
-                // Should we silently ignore the case where `result` and `error` are both nil, or should this be an error situation?
+        return Observable.create { [weak base] (subscriber) -> Disposable in
+            let cancellable = base?.fetch(query: query, cachePolicy: cachePolicy, context: nil, queue: queue) { result in
+                NetworkActivityIndicatorManager.shared.decrementActivityCount()
+                switch result {
+                case .success(let result):
+                    if let errors = result.errors {
+                        subscriber.onError((RxApolloError.graphQLErrors(errors)))
+                    } else if let data = result.data {
+                        subscriber.onNext(data)
+
+                        if cachePolicy == .returnCacheDataAndFetch {
+                            if result.source == .server {
+                                subscriber.onCompleted()
+                            }
+                        } else {
+                            subscriber.onCompleted()
+                        }
+                    } else {
+                        subscriber.onError(RxApolloError.unknown)
+                    }
+                case .failure(let error):
+                  // Network or response format errors
+                  subscriber.onError(error)
+                }
             }
 
             return Disposables.create {
-                watcher.cancel()
+                cancellable?.cancel()
             }
         }
     }
@@ -89,29 +71,32 @@ public final class ApolloReactiveExtensions {
     /// - Parameters:
     ///   - mutation: The mutation to perform.
     ///   - queue: A dispatch queue on which the result handler will be called. Defaults to the main queue.
-    /// - Returns: A `Maybe` that emits the results of the mutation.
-    public func perform<Mutation: GraphQLMutation>(mutation: Mutation, queue: DispatchQueue = DispatchQueue.main) -> Maybe<Mutation.Data> {
-        return Maybe.create { maybe in
-            let cancellable = self.client.perform(mutation: mutation, queue: queue) { result, error in
-                if let error = error {
-                    maybe(.error(error))
-                } else if let errors = result?.errors {
-                    maybe(.error(RxApolloError.graphQLErrors(errors)))
-                } else if let data = result?.data {
-                    maybe(.success(data))
-                } else {
-                    maybe(.completed)
+    /// - Returns: A `Single` that emits the results of the mutation.
+    public func perform<Mutation: GraphQLMutation>(mutation: Mutation, queue: DispatchQueue = DispatchQueue.main) -> Single<Mutation.Data> {
+        NetworkActivityIndicatorManager.shared.incrementActivityCount()
+        return Single.create { [weak base] single in
+            let cancellable = base?.perform(mutation: mutation, context: nil, queue: queue) { (result) in
+                NetworkActivityIndicatorManager.shared.decrementActivityCount()
+                switch result {
+                case .success(let result):
+                    if let errors = result.errors {
+                        single(.error(RxApolloError.graphQLErrors(errors)))
+                    } else if let data = result.data {
+                        single(.success(data))
+                    } else {
+                        single(.error(RxApolloError.unknown))
+                    }
+                case .failure(let error):
+                  // Network or response format errors
+                  single(.error(error))
                 }
             }
 
             return Disposables.create {
-                cancellable.cancel()
+                cancellable?.cancel()
             }
         }
     }
 }
 
-public extension ApolloClient {
-    /// Reactive extensions.
-    var rx: ApolloReactiveExtensions { return ApolloReactiveExtensions(self) }
-}
+extension ApolloClient: ReactiveCompatible {}
